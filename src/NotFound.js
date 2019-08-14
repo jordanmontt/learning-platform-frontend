f77ec646d44083fd9fb8e3bd27ec0516aab4756b
jmsebastian <sebastianjmt@hotmail.com>
2019-08-14 11:38:40 -0400

    added internal routing and not found component when a url doesnt exist

---
+++ src/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <br />
+                <br />
+                <h1 className="title is-3 has-text-centered has-text-dark">
+                    La página solicitada no existe
+                </h1>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import Login from './Login';
+import SignUp from './SignUp';
+import NotFound from './NotFound';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route component={NotFound} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
